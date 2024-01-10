fd8f0005dc15f7f83159d06701aa84db0ad653d2
Kursywa <masterortografii@gmail.com>
2024-01-10 18:15:43 +0100

    code finished

--- ZadanieIIIMR/Main.cpp
+++ ZadanieIIIMR/Main.cpp
@@ -1,9 +1,9 @@
-﻿
-#include<iostream>
+﻿#include<iostream>
 #include<fstream>
 #include<vector>
 #include<string>
 #include<sstream>
+#include<unordered_map>
 
 using namespace std;
 
@@ -29,6 +29,8 @@ void read_fasta_data(vector <char>(&fasta)[5], string file_name);
 void read_qual_data(vector<int>(&qual)[5], string file_name);
 void delete_below_threshold(vector<char>(&fasta)[5], vector<int>(&qual)[5], vector<char>(&sequence)[5], vector<int>(&input_sequence_index)[5], int threshold);
 void create_graph(vector<vector<Vertex>> &graph, vector<char> sequence[5], vector<int> input_sequence_index[5], int substring_length);
+void connect_edges(vector<vector<Vertex>>& graph, int substring_length);
+vector<Vertex> find_result(vector<vector<Vertex>>graph);
 
 //main project
 int main() {
@@ -39,7 +41,6 @@ int main() {
 	vector<char> fasta[5];
 	vector<int> qual[5];
 	string line;
-//new vector<char> holding chars after threshold rejection and indexes of these chars from original sequence 
 	vector<char> sequence[5];
 	vector<int> input_sequence_index[5];
 	vector<vector<Vertex>> graph; 
@@ -51,36 +52,27 @@ int main() {
 	read_fasta_data(fasta, file_name + ".fasta"); //reading data from fasta file
 	read_qual_data(qual, file_name + ".qual"); //  reading data from qual file
 
-	delete_below_threshold(fasta, qual, sequence, input_sequence_index, threshold); //deleting nucleotides form sequence below threshold
+	delete_below_threshold(fasta, qual, sequence, input_sequence_index, threshold); //deleting nucleotides from sequence below threshold
 	// the data is now stored in vector of chars along with corresponding vector of input_sequence_index
 
-	///struktura/klasa przechowująca numer pierwszego nukleotydu w oligonukleotydzie, sekwencje oligonukleotydową i (ewentualnie) numer sekwencji z kórej on pochodzi 
-	//funkcja dodająca do tej struktury te oligonukleotydy o długości substring_length jedne po drugim eg: 1ACTG 2CTGT ...
-	// porównanie i znalezienie tych oligonukleotydów pomiędzy sekwencjami jeżeli odległość między numerem pierwszego nukleotydu jest
-	//  nie większa od substring_length * 10
+	create_graph(graph, sequence, input_sequence_index, substring_length); // creating graph where vertex is an oligonucleotide sequence
+
+	// connecting vertices using edges
+	connect_edges(graph, substring_length);
+
+	// find motive using naive algorithm
+	vector<Vertex> result = find_result(graph);
 	
-	create_graph(graph, sequence, input_sequence_index, substring_length);
-	//for (int i = 0; i < 5; i++) { // for each sequence
-	//	int counter = 0; //at which nucleotide from sequence we currently are in
-	//	while (counter < (sequence[i].size() - (substring_length - 1))){	//Do as many times so you exhaust whole sequence, beware of substring frame
-	//		Vertex oligonucleotide;
-	//		oligonucleotide.first_index = input_sequence_index[i][counter]; // i - sequence number, counter - index of first nucleotide
-	//		oligonucleotide.nr_seq = i; 
-	//		for (int k = 0; k < substring_length; k++) { // iterate as many times as there is substring_length
-	//			oligonucleotide.oligo_sequence += sequence[i].at(counter++);
-	//		}
-	//		counter -= (substring_length - 1); // counter is set to one place higher after this operation as we progress with counter by increasing it by
-	//		// one nucleotide
-	//		graph.push_back(oligonucleotide);
-	//	}
-	//}
-	// new function that works as "graph" but Vertices on appropriate first layer of vector 
-	
-	int sum = 0; // iterator for all vertices in all sequences - may be redundant
-	for (int i = 0; i < 5; i++) {
-		sum += input_sequence_index->size();
+	//wypisanie rezultatu na wyjściu w postaci: nr sekwencji wejściowej, 
+	// nr pozycji w tej sekwencji, dla każdego podciągu wchodzącego w skład kliki (struktury zbliżonej do kliki)
+	// oraz sekwencję nukleotydową podciągu
+
+	cout << endl << "results: " << endl;
+	for (const auto& vertex : result) {
+			cout << vertex << endl;
 	}
-	 
+		cout << endl;
+
 	//test
 	//cout << substring_length << endl;
 	//for (int i = 0; i < 5; i++) {
@@ -118,25 +110,25 @@ int main() {
 	//	std::cout << std::endl;
 	//}
 
-	for (int i = 0; i < 5; i++) {
-		cout << "instance [" << i << "]" << endl;
-		for (int j = 0; j < fasta[i].size(); j++) {
-			cout << "fasta: " << fasta[i][j] << "(" << qual[i][j] << ")[" << j + 1 << "] ";
-		}
-		cout << endl << "sequence[" << i << "]\tthreshold: " << threshold << endl;
-		for (int k = 0; k < sequence[i].size(); k++) {
-			cout << "char: " << sequence[i][k] << "[" << input_sequence_index[i][k] << "]" << "\t";
-		}
-		cout << endl;
+	//for (int i = 0; i < 5; i++) {
+	//	cout << "instance [" << i << "]" << endl;
+	//	for (int j = 0; j < fasta[i].size(); j++) {
+	//		cout << "fasta: " << fasta[i][j] << "(" << qual[i][j] << ")[" << j + 1 << "] ";
+	//	}
+	//	cout << endl << "sequence[" << i << "]\tthreshold: " << threshold << endl;
+	//	for (int k = 0; k < sequence[i].size(); k++) {
+	//		cout << "char: " << sequence[i][k] << "[" << input_sequence_index[i][k] << "]" << "\t";
+	//	}
+	//	cout << endl;
 
-	}
+	//}
 
-	for (const auto& vertices : graph) {
-		for (const auto& vertex : vertices) {
-			cout << vertex << " ";
-		}
-		cout << endl;
-	}
+	//for (const auto& vertices : graph) {
+	//	for (const auto& vertex : vertices) {
+	//		cout << vertex << " ";
+	//	}
+	//	cout << endl;
+	//}
 
 }
 
@@ -251,6 +243,53 @@ void create_graph(vector<vector<Vertex>>&graph, vector<char> sequence[5], vector
 			counter -= (substring_length - 1); // counter is set to one place higher after this operation as we progress with counter by increasing it by
 			// one nucleotide
 			graph.emplace_back(vector<Vertex>{oligonucleotide});
+		}
+	}
+}
+
+void connect_edges(vector<vector<Vertex> >& graph, int substring_length) {
+	for (int i = 0; i < (graph.size() - 1); i++) { // "-1" so it wont compare the last element to non-existing element of this vector
+		for (int j = i + 1; j < graph.size(); j++) {
+			int distance = 0; // distance between oligonucleotides
+			if (graph[i][0].first_index > graph[j][0].first_index) {
+				distance = graph[i][0].first_index - graph[j][0].first_index;
+			}
+			else {
+				distance = graph[j][0].first_index - graph[i][0].first_index;
+			}
+			if (graph[i][0].oligo_sequence == graph[j][0].oligo_sequence // if oligosequences are aligned
+				&& graph[i][0].nr_seq != graph[j][0].nr_seq  // compare different sequences
+				&& distance <= (substring_length * 10)) { // if oligonucleotides arent too far from each other
+				graph[i].push_back(graph[j][0]); // graph[i][0] have edges to -> graph[i][j]
+				graph[j].push_back(graph[i][0]);
+			}
+		}
+	}
+}
+
+vector<Vertex> find_result(vector<vector<Vertex>>graph) {
+	vector<Vertex> result;
+	for (const auto& outerVec : graph) {
+		unordered_map<int, Vertex> nrSeqToVertexMap;
+
+		if (!result.empty()) {
+			return result;
+		}
+		for (const auto& vertex : outerVec) {
+			// Check if nr_seq is within the required range and is not already found
+			if (nrSeqToVertexMap.count(vertex.nr_seq) == 0) {
+				nrSeqToVertexMap[vertex.nr_seq] = vertex;
+			}
+
+			// Check if we have found one vertex for each nr_seq value
+			if (nrSeqToVertexMap.size() == 5) {
+
+				for (int i = 0; i < 5; ++i) {
+					result.push_back(nrSeqToVertexMap[i]);
+				}
+
+				break; // No need to check further for this outer vector
+			}
 		}
 	}
 }
